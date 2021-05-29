4e69224174d1951884f1a26e45e7a61153476b3d
filipe santos <fhilips.souza@gmail.com>
2021-05-29 19:01:51 -0300

    add telas.gif

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
         <p className="text-light">
           App desenvolvido por{" "}
           <a
-            href="https://github.com/acenelio"
+            href="https://github.com/fhilips"
             target="_blank"
             rel="noreferrer"
           >
